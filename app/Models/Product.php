8b8748257c326ff8a34f5d99c027d28ea3f2de3e
Michael McNamara <mihail.macnamara@gmail.com>
2025-04-16 13:10:11 +0300

    fix join querry

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -19,19 +19,23 @@ class Product extends Model
 
         $category_id = $category_id == 'all' ? null : $category_id;
         $manufacturer_id = $manufacturer_id == 'all' ? null : $manufacturer_id;
-        return self::query()
-            ->select('quantity','identifier', 'name', 'category_id')
-            //  ->where('stock_status_id', 7)
-            ->leftJoin('oc_product_description', 'oc_product.product_id', '=', 'oc_product_description.product_id')
-            ->leftJoin('oc_product_to_category', 'oc_product.product_id', '=', 'oc_product_to_category.product_id')
-            ->where('oc_product_description.language_id', 1)
-            ->when($category_id, function ($query) use ($category_id) {
-                return $query->where('category_id', $category_id);
-            })
-            ->when($manufacturer_id, function ($query) use ($manufacturer_id) {
-                return $query->where('manufacturer_id', $manufacturer_id);
-            })
-            ->get()
-            ->toArray();
+
+        $query = self::query()
+            ->select('oc_product.product_id', 'quantity', 'identifier', 'name');
+
+            if ($category_id) {
+                $query->leftJoin('oc_product_to_category', 'oc_product.product_id', '=', 'oc_product_to_category.product_id')
+                    ->where('oc_product_to_category.category_id', $category_id);
+            }
+            $query->leftJoin('oc_product_description', 'oc_product.product_id', '=', 'oc_product_description.product_id')
+                ->where('oc_product_description.language_id', 1)
+                ->when($manufacturer_id, function ($query) use ($manufacturer_id) {
+                    return $query->where('manufacturer_id', $manufacturer_id);
+                });
+
+
+        $products = $query->get()->toArray();
+
+        return $products;
     }
 }
