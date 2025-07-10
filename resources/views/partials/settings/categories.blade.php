673de560ee16fdc396ab1c1893f9010001a91068
Michael McNamara <mihail.macnamara@gmail.com>
2025-07-10 17:07:01 +0300

    Миграции таблиц настроек

--- resources/views/partials/settings/categories.blade.php
+++ resources/views/partials/settings/categories.blade.php
@@ -1,1 +1,7 @@
-<h1>categories</h1>
+<button type="button" class="btn btn-secondary">Обновить категории</button>
+<div class="mt-3">
+    <div class="form-check form-switch">
+        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+        <label class="form-check-label" for="flexSwitchCheckDefault">Включить что-то</label>
+    </div>
+</div>
--- resources/views/partials/settings/manufacturers.blade.php
+++ resources/views/partials/settings/manufacturers.blade.php
@@ -1,4 +1,7 @@
-<div class="form-check form-switch">
-    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
-    <label class="form-check-label" for="flexSwitchCheckDefault">Включить что-то</label>
+<button type="button" class="btn btn-secondary">Обновить производителей</button>
+<div class="mt-3">
+    <div class="form-check form-switch">
+        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+        <label class="form-check-label" for="flexSwitchCheckDefault">Включить что-то</label>
+    </div>
 </div>
